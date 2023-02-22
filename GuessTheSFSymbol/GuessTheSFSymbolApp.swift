e5379340a2e2a4da74d4dbd65659088b65e5bae6
Zoe Cutler <zcutlerf@gmail.com>
2023-02-22 13:51:36 -0500

    Initial Commit

---
+++ GuessTheSFSymbol/GuessTheSFSymbolApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheSFSymbolApp.swift
+//  GuessTheSFSymbol
+//
+//  Created by Zoe Cutler on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheSFSymbolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
